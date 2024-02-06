0398a4a028d75e4a2656b7f9b3f178d1c953b379
jeremy.surget <jeremysurget77@gmail.com>
2024-02-06 17:37:55 +0100

    config var for instance type

--- stack/pulumi/__main__.py
+++ stack/pulumi/__main__.py
@@ -1,13 +1,23 @@
+import pulumi
 import instance
 import network
 import security_groups
 import vpc_endpoints
 import data_warehouse
 
+cfg = pulumi.Config()
 
-test_instance = instance.ec2_instance(
-    resource_name="test-instance",
-    instance_type="t3.micro",
+metabase_instance = instance.ec2_instance(
+    resource_name="metabase_instance",
+    instance_type=cfg.require("metabase_instance_type"),
+    az="eu-west-3a",
+    subnet_id=network.private_subnet.id,
+    security_group_ids=[security_groups.sg_ssm.id]
+)
+
+airbyte_instance = instance.ec2_instance(
+    resource_name="airbyte_instance",
+    instance_type=cfg.require("airbyte_instance_type"),
     az="eu-west-3a",
     subnet_id=network.private_subnet.id,
     security_group_ids=[security_groups.sg_ssm.id]
